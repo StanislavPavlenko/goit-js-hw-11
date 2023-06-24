ed43136e6c2b6f7b17fdb77eeef30b41ffed2853
StanislavPavlenko <voltfrag@gmail.com>
2023-06-24 23:06:09 +0300

    12

---
+++ src/index.js
@@ -0,0 +1,128 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import throttle from 'lodash.throttle';
+import NewServer from './js/pixa-api';
+const newServer = new NewServer();
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const textResult = document.querySelector('.text');
+const searchForm = document.querySelector('.search-form');
+searchForm.addEventListener('submit, onFormSubmit');
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    showLoader();
+    clearContainer();
+    textResult.style.display = 'none';
+    newServer.query = e.currentTarget.elemets.searchQuery.value;
+    newServer.resetPage();
+    setTimeout(() => {
+        newServer
+            .fetchSearch()
+            .then(request => {
+                hidenLoader();
+                if (newServer.query === '') {
+                    return;
+                } else if (request.hits.length === 0) {
+                    return Notify.failure(
+                        'Sorry, there are no images matching your search query. Please try again.'
+                    );
+                }
+                Notify.success(`Hooray! We found ${request.totalHits} images.`);
+                renderResult(request.hits), lightbox.refresh();
+            })
+            .catch(err => {
+                hidenLoader()
+                console.log(err);
+                Notify.failure(
+                    'Sorry, there are no images matching your search query. Please try again.'
+                );
+            });
+    }, 500);
+}
+
+function renderResult(arry) {
+  const markup = arry
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<div class="photo-card">
+        <a href="${largeImageURL}"><img width="100%" height= "200" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        <div class="info">
+            <p class="info-item">
+              <b>Likes</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+            ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+            ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+            ${downloads}
+            </p>
+        </div>
+    </div>`
+    )
+    .join('');
+  return gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearContainer() {
+  gallery.innerHTML = '';
+}
+
+window.addEventListener('scroll', throttle(handleScroll, 500));
+
+function handleScroll() {
+  showLoader();
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    if (newServer.query === '') {
+      return;
+    }
+    if (newServer.numberPage > newServer.totalPages()) {
+      textFinish.style.display = 'block';
+      hidenLoader() 
+      return;
+    }
+    setTimeout(() => {
+      newServer
+        .fetchSearch()
+        .then(request => {
+          renderResult(request.hits), lightbox.refresh();
+          const { height: cardHeight } = document
+            .querySelector('.gallery')
+            .firstElementChild.getBoundingClientRect();
+
+          window.scrollBy({
+            top: cardHeight * 2,
+            behavior: 'smooth',
+          });
+          hidenLoader();
+        })
+        .catch(err => {
+          hidenLoader()
+          console.log(err);
+          Notify.failure(`An error occurred during the search.`);
+        });
+    }, 500);
+  }
+}
+
+function showLoader() {
+  loader.style.display = 'block';
+}
+
+function hidenLoader() {
+  loader.style.display = 'none';
+}
---
+++ src/js/pixa-api.js
@@ -0,0 +1,52 @@
+const axios = require('axios/dist/browser/axios.cjs');
+
+export default class NewServer {
+  constructor() {
+    this.searchName = '';
+    this.numberPage = 1;
+  }
+  async fetchSearch() {
+    const BASE_URL = 'https://pixabay.com/api/';
+    const API_KEY = '37071230-d6b04d3068f1a0950a5b376a5';
+
+    const searchParams = new URLSearchParams({
+      key: API_KEY,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      q: this.searchName,
+      page: this.numberPage,
+    });
+
+    try {
+      const response = await axios.get(`${BASE_URL}?${searchParams}`);
+      this.data = response.data;
+      this.inctementPage();
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      new Error('An error occurred during the search.');
+      return;
+    }
+  }
+  inctementPage() {
+    this.numberPage += 1;
+  }
+
+  resetPage() {
+    this.numberPage = 1;
+  }
+
+  get query() {
+    return this.searchName;
+  }
+
+  set query(newQuery) {
+    return (this.searchName = newQuery);
+  }
+
+  totalPages() {
+    return Math.ceil(this.data.totalHits / 40);
+  }
+}
